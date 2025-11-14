0abf314aa0e07fab2ac8d5bdf9e87e8b3b62ebbb
javiercoronadonarvaez <javier.coronado@metalab.com>
2025-11-14 17:22:39 -0600

    feat: add FE for fetching image from persistent storage

--- todo_frontend/src/App.tsx
+++ todo_frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import LocalImage from "./components/LocalImage";
+
 const App = () => {
   return (
     <div>
-      <p>Hello world</p>
+      <h1>The Project App</h1>
+      <>
+        <LocalImage />
+      </>
     </div>
   );
 };
