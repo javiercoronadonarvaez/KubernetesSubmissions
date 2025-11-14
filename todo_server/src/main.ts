f75df6076f038b3ddb0c00a2cea90cb46510830e
javiercoronadonarvaez <javier.coronado@metalab.com>
2025-11-14 16:29:00 -0600

    feat: add image fetch and write process upon initialising

--- todo_server/src/main.ts
+++ todo_server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { ImageService } from './services/image.service'; // Import your service
 import 'dotenv/config';
 
 const bootstrap = async (): Promise<void> => {
@@ -9,5 +10,23 @@ const bootstrap = async (): Promise<void> => {
   await app.listen(port);
   const logger = new Logger('Bootstrap');
   logger.log(`Server started on port ${port}`);
+
+  // Get the ImageService instance from the app context
+  const imageService = app.get(ImageService);
+  await imageService.getRemoteImageStream();
+  logger.log('Fetched and saved remote image ON STARTUP');
+
+  // Call getRemoteImageStream every 10 seconds
+  setInterval(() => {
+    void (async () => {
+      try {
+        await imageService.getRemoteImageStream();
+        logger.log('Fetched and saved remote image');
+      } catch (err) {
+        logger.error('Failed to fetch remote image', err);
+      }
+    })();
+  }, 10000); // adjust interval as needed
 };
+
 bootstrap().catch(console.error);
