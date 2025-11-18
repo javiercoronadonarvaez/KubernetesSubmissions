a7da5f67cd02a4be27d07943f1d62bd66fcbc799
javiercoronadonarvaez <javier.coronado@metalab.com>
2025-11-17 19:31:41 -0600

    feat: add todo controller for GET and POST requests

--- todo_server/src/app.module.ts
+++ todo_server/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 import { ImageController } from './controllers/image.controller';
 import { ImageService } from './services/image.service';
+import { TodoService } from './services/todo.service';
+import { TodoController } from './controllers/todo.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ImageController],
-  providers: [AppService, ImageService],
+  controllers: [AppController, ImageController, TodoController],
+  providers: [AppService, ImageService, TodoService],
 })
 export class AppModule {}
