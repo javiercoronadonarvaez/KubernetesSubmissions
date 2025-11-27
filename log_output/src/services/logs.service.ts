a90b359f7ce4803daa1501b0c909b7b357aea4b4
javiercoronadonarvaez <javier.coronado@metalab.com>
2025-11-27 10:05:11 -0600

    feat: add info file from configMap onto response message

--- log_output/src/services/logs.service.ts
+++ log_output/src/services/logs.service.ts
@@ -11,17 +11,21 @@ const AXIOS_PING_BACKEND_URL =
 export class LogsService {
   private directory = path.join('/', 'usr', 'src', 'app', 'files');
   private logsFilePath = path.join(this.directory, 'log_output.txt');
-  // private pingPongFilePath = path.join(this.directory, 'ping_pong_output.txt');
+  private configDirectory = path.join('/', 'usr', 'src', 'app', 'config');
+  private configFilePath = path.join(this.configDirectory, 'information.txt');
+
   private getPingPongMessage = async (): Promise<string> => {
     const response = await axios.get(AXIOS_PING_BACKEND_URL);
     return response.data as string;
   };
 
   public getRandomString = async (): Promise<string> => {
+    const informationTextFile = await getFile(this.configFilePath);
+    const informationTextMessage = getLastLine(informationTextFile);
     const textFile = await getFile(this.logsFilePath);
     const lastLine = getLastLine(textFile);
     const pingPongMessage = await this.getPingPongMessage();
-    const combinedMessage = `${lastLine}\nPing Pong Message: ${pingPongMessage}`;
+    const combinedMessage = `file content: ${informationTextMessage}\n${lastLine}\nPing Pong Message: ${pingPongMessage}`;
     return combinedMessage;
   };
 }
